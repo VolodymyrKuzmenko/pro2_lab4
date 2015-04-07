452d3f7f094fcc8490f47f761e4ab387b5dd5bb1
Volodimir Kuzmenko <kuzmenko.volodimir@gmail.com>
2015-04-07 18:31:35 +0300

    https://github.com/VolodymyrKuzmenko/pro2_lab4.git

---
+++ pro2_lab4/operations.h
@@ -0,0 +1,29 @@
+﻿/**
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*                                                             *
+*            Програмування паралельний комп'ютерних сисем     *
+*             Лабораторна робота №4. Бібліотека OpenMP        *
+*                                                             *
+* Завдання: A = (B∙MO)∙(MK∙MR) + α∙E                          *
+*                                                             *
+* Автор Кузьменко Володимир                                   *
+* Група ІО-21                                                 *
+* Дата 07.04.15                                               *
+*                                                             *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+*/
+
+
+typedef int* vector;
+typedef int** matrix;
+
+const int N = 4;
+
+vector inputVector(int);
+matrix inputMatrix(int);
+void output(vector);
+void output(matrix);
+vector copyVector(vector);
+matrix copyMatrix(matrix);
+matrix initMatrix();
+vector initVector();
